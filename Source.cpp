b90da4720c540ee80c12cc9e39cc9d17d223b76b
sonyakostyukevich22 <43521365+sonyakostyukevich22@users.noreply.github.com>
2019-01-10 23:32:54 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+7
+int f()
+{
+	int arr[9]{ 4, 2, 6, 7, 6, 8, 10, 1, 6 };
+	for (int i = 0; i < 9; ++i)
+	{
+		if (arr[i + 1] < arr[1] && arr[i + 1] < arr[i + 2])
+		{
+			dest[i] = arr[i];
+		}
+	}
+	for (int j = 0; j < dest[i]; ++j)
+}
+
+
+
+//4 2 6 7 6 8 10 1 6
+int main()
+{
+
+
+	std::cout << f() << std::endl;
+	system("pause");
+	return 0;
+}
